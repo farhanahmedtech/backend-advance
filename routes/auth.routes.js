d81f942e47b4519106d1f0f710673912ca86cfa1
Farhan Ahmed <farhanahmedmirali@gmail.com>
2026-02-24 08:24:12 +0500

    auth routes with login logout

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,8 +1,10 @@
 import express, { Router } from "express"
-import { signUp } from "../controllers/auth.controllers.js"
+import { login, logOut, signUp } from "../controllers/auth.controllers.js"
 
 const authRouter = express.Router()
 
 authRouter.post("/signup", signUp)
+authRouter.post("/login", login)
+authRouter.post("/logout", logOut)
 
 export default authRouter
